0cbcca676ccd37cf18a5ee9a107a6eb1915d2497
Dipanshu Verma <dipanshu231099@gmail.com>
2020-07-02 17:31:37 +0530

    added acknowledgement.php

---
+++ acknowledgement.php
@@ -0,0 +1,74 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Developers</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" id="font-awesome">
+    <link rel="stylesheet" href="stylesheets/style.css">
+</head>
+<body style="background-image:url(public/bg-image.jpg)">
+<div class="container py-5">
+    <div class="row text-center text-white">
+        <div class="col-lg-8 mx-auto">
+            <u><h1 class="display-4">The Team</h1></u>
+        </div>
+    </div>
+</div><!-- End -->
+<div class="container">
+    <div class="row text-center">
+        <!-- Team item -->
+        <div class="col-xl-3 col-sm-6 mb-5">
+            <div class="bg-white rounded shadow-sm py-5 px-4"><img src="public/varun.jpeg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+                <h5 class="mb-0">Varun Dutt</h5><span class="small text-uppercase text-muted">Mentor</span>
+                <br>
+                <span class="small text-uppercase text-muted">Associate Professor <br> IIT Mandi</span>
+
+                <ul class="social mb-0 list-inline mt-3">
+                    <li class="list-inline-item"><a href="http://faculty.iitmandi.ac.in/~varun/" class="social-link"><i class="fa fa-suitcase"></i></a></li>
+                </ul>
+            </div>
+        </div><!-- End -->
+
+        <!-- Team item -->
+        <div class="col-xl-3 col-sm-6 mb-5">
+            <div class="bg-white rounded shadow-sm py-5 px-4"><img src="public/dipanshu.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+                <h5 class="mb-0">Dipanshu</h5><span class="small text-uppercase text-muted">Application Developer</span>
+                <br>
+                <span class="small text-uppercase text-muted">Sophomore BTech <br> IIT Mandi</span>
+                <ul class="social mb-0 list-inline mt-3">
+                    <li class="list-inline-item"><a href="https://www.linkedin.com/in/dipanshu-verma-955068183/" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+                </ul>
+            </div>
+        </div><!-- End -->
+
+        <!-- Team item -->
+        <div class="col-xl-3 col-sm-6 mb-5">
+            <div class="bg-white rounded shadow-sm py-5 px-4"><img src="public/ayushman.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+                <h5 class="mb-0">Ayushman</h5><span class="small text-uppercase text-muted">Application Developer</span>
+                <br>
+                <span class="small text-uppercase text-muted">Sophomore BTech <br> IIT Mandi</span>
+                <ul class="social mb-0 list-inline mt-3">
+                    <li class="list-inline-item"><a href="https://www.linkedin.com/in/ayushman-dixit-4812b9171/" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+                </ul>
+            </div>
+        </div><!-- End -->
+
+        <!-- Team item -->
+        <div class="col-xl-3 col-sm-6 mb-5">
+            <div class="bg-white rounded shadow-sm py-5 px-4"><img src="public/shruti.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+                <h5 class="mb-0">Shruti Kaushik</h5><span class="small text-uppercase text-muted">Guidance</span><br>
+                <span class="small text-uppercase text-muted">PHD <br> IIT Mandi</span>
+                <ul class="social mb-0 list-inline mt-3">
+                    <li class="list-inline-item"><a href="https://www.linkedin.com/in/shruti-kaushik-799977b8/" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+                </ul>
+            </div>
+        </div><!-- End -->
+    </div>
+</div>
+</body>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+</html>
+
